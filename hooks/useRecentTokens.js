6bfaba448f2f4e271ee2d24fe4d5ed553abecee1
kolber <hi@kolber.info>
2022-03-02 14:36:27 +1100

    Fixes for useRecentTokens to handle single items properly

--- hooks/useRecentTokens.js
+++ hooks/useRecentTokens.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react'
 
-export const useRecentTokens = ({ url, start, end, reverse }) => {
+export const useRecentTokens = ({ url, start = 0, end = 0, reverse = false }) => {
   const [isLoading, setIsLoading] = useState(true)
-  const [tokens, setTokens] = useState({})
+  const [tokens, setTokens] = useState([])
 
   const getTokens = async ({ url, start, end, reverse }) => {
     // react-query / cache this part?
-    const tokens = await fetch(`${url}${start}...${end}`).then((r) => r.json())
+    let tokens = []
+    try {
+      tokens = await fetch(`${url}${start}...${end}`).then((r) => r.json())
+      if (tokens.error) tokens = []
+      if (!Array.isArray(tokens)) tokens = [tokens]
+    } catch (e) {}
     setTokens(reverse ? tokens.reverse() : tokens)
     setIsLoading(false)
   }
